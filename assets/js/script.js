41dab3652b6f66712f663d967c2b31fed82c5895
Enrique Jimenez <ejimenez2292@gmail.com>
2022-02-26 22:55:53 -0600

    struggling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,31 @@
 var playNow = document.getElementById("play");
 var questionList = document.getElementById("quiz-questions");
 var nextButton = document.getElementById("next");
-var questions = document.getElementById("questions");
-var answers = document.getElementById("answers");
+var questionEl = document.getElementById("questions");
+var choicesEl = document.getElementById("answers");
+//var choice1 = document.getElementById("choice1");
+//var choice2 = document.getElementById("choice2");
+//var choice3 = document.getElementById("choice3");
+
+var currentQuestionIndex = 0;
+
 
 playNow.addEventListener("click", startQuiz)
-nextButton.addEventListener("click", () => {
-    quizQuestions++
-    questionList
-}) 
 
 
 function startQuiz () {
     playNow.classList.add("hide");
     questionList.classList.remove("hide");
-    questionsList();
+    questionsList(currentQuestionIndex);
 }
 
 function questionsList () {
-    addQuestions(quizQuestions);
-}
+    questionEl.innerText = quizQuestions[0].question;
+    quizQuestions.choices.forEach(choices => {
+        var button = document.createElement("button")
+        button.innerHTML = choices.text
+    })
 
-function addQuestions () {
-    questions.innerHTML = quizQuestions[0].question;
-    document.getElementById("answers").innerText = {
-        choices
-    }
 }
 
 
@@ -33,28 +33,30 @@ function addQuestions () {
 var quizQuestions = [
     {
       question: "What is an HTML Iframe used for?",
-      choices: ["Frame Photos in your document", "Embed a document within your HTML", "Index your HTML 'p' tag for proper indentation"],
-      answer: "Embed a document within your HTML"
-    },
-    {
-      question: "Flexbox is used for which of the following purposes?",
-      choices: ["Designing flexible responsive layouts without using float or positioning", "Automatically setting the height and width of a property","Adds a line-break after each element, so they can sit next to other elements"], 
-      answer: "Designing flexible responsive layouts without using float or positioning",
-    },
-    {
-        question: "True or False: Media Queries allow a deployed website to adapt to multiple viewports?",
-        choices: ["True", "False", "Bro...what are you talking about?"],
-        answer: "True"
-    },
-    {
-        question: "Please identify the proper syntax for an Array.",
-        choices: ["var car = Ford, Chevy, Dodge;", "var car = 'Ford' 'Chevy' 'Dodge';", "var car = ['Ford','Chevy','Dodge'];"],
-        answer: "var car = ['Ford','Chevy','Dodge'];"
-    },
-    {
-        question: "What is the definition of 'THIS'",
-        choices: ["THIS is a quiz","THIS is a programming language","THIS is the object from which the method was called"],
-        answer: "THIS is the object from which the method was called"
+      choices: [
+          {text: "Frame Photos in your document", correct: false},
+          {text: "Embed a document within your HTML", correct: true},
+          {text: "Index your HTML 'p' tag for proper indentation", correct: false}
+      ]
     }
-];
-
+   // {
+     // question: "Flexbox is used for which of the following purposes?",
+      //choices: ["Designing flexible responsive layouts without using float or positioning", "Automatically setting the height and width of a property", "Adds a line-break after each element, so they can sit next to other elements"], 
+      //answer: "Designing flexible responsive layouts without using float or positioning",
+    //},
+    //{
+      //  question: "True or False: Media Queries allow a deployed website to adapt to multiple viewports?",
+        //choices: ["True", "False", "Bro...what are you talking about?"],
+        //answer: "True"
+    //},
+    //{
+      //  question: "Please identify the proper syntax for an Array.",
+        //choices: ["var car = Ford, Chevy, Dodge;", "var car = 'Ford' 'Chevy' 'Dodge';", "var car = ['Ford','Chevy','Dodge'];"],
+        //answer: "var car = ['Ford','Chevy','Dodge'];"
+    //},
+    //{
+      //  question: "What is the definition of 'THIS'",
+        //choices: ["THIS is a quiz", "THIS is a programming language", "THIS is the object from which the method was called"],
+        //answer: "THIS is the object from which the method was called"
+    //}
+]
