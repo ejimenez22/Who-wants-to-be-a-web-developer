f8a2b7b0bc4b5d17696ab2a93a0bbb6025ab7c4a
Enrique Jimenez <ejimenez2292@gmail.com>
2022-02-27 14:17:52 -0600

    added css styling to choicebtn/changed the true and false choices

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,7 @@ var questionList = document.getElementById("quiz-questions");
 var nextButton = document.getElementById("next");
 var questionEl = document.getElementById("questions");
 var choicesEl = document.getElementById("buttonContainer");
+var choicesBtns = document.querySelector("choice-btn")
 
 var currentQuestionIndex = 0;
 
@@ -13,78 +14,85 @@ nextButton.addEventListener("click", () => {
     questionsList()
 })
 
-
-function startQuiz () {
+function startQuiz() {
     playNow.classList.add("hide");
     questionList.classList.remove("hide");
     questionsList(currentQuestionIndex);
-    //reset ()
+    choicesBtns.forEach(element => element.classList.add("hide"));
 }
 
-function questionsList () {
-    questionEl.innerText = quizQuestions[currentQuestionIndex].question;
-    for (var i = 0; i < 3; i++) {
-        var newButton = document.createElement("button");
-        newButton.style.backgroundColor = "#8F00FF"
-        newButton.style.fontSize = "40px"
-        newButton.style.color = "white"
-        newButton.innerText = quizQuestions[currentQuestionIndex].choices[i].text;
-        document.getElementById("buttonContainer").appendChild(newButton)
-        if (quizQuestions[currentQuestionIndex].choices[i].correct)
-        {
-            newButton.addEventListener ("click", (e) => {
-            newButton.style.backgroundColor = "#228B22" })}
-            else {
-            newButton.addEventListener ("click", (e) => {
-                newButton.style.backgroundColor = "#8b0000" 
-            })
-        }
+function choiceClick() {
+    console.log(this)
+    console.log(this.value)
+    if (this.value == "true") {
+        this.classList.add("correct")
+    } else {
+        this.classList.add("wrong")
     }
 }
 
-    
+function questionsList() {
+    questionEl.innerText = quizQuestions[currentQuestionIndex].question;
+    for (var i = 0; i < 3; i++) {
+        var newButton = document.createElement("button");
+        newButton.setAttribute("value", quizQuestions[currentQuestionIndex].choices[i].correct);
+        newButton.setAttribute("class", "choice-btn");
+        newButton.textContent = quizQuestions[currentQuestionIndex].choices[i].text;
+
+
+        newButton.onclick = choiceClick
+        document.getElementById("buttonContainer").appendChild(newButton)
+    }
+}
+
+
 
 
 // Questions 
 var quizQuestions = [
     {
-      question: "What is an HTML Iframe used for?",
-      choices: [
-          {text:"Frame Photos in your document", correct: false},
-          {text: "Embed a document within your HTML", correct: true},
-          {text: "Index your HTML 'p' tag for proper indentation", correct: false}
-      ]
-    },
-   {
-      question: "Flexbox is used for which of the following purposes?",
-      choices: [
-          {text: "Designing flexible responsive layouts without using float or positioning", correct: true},
-          {text: "Automatically setting the height and width of a property", correct: false},
-          {text: "Adds a line-break after each element, so they can sit next to other elements", correct: false}
-      ]
+        question: "What is an HTML Iframe used for?",
+        choices: [
+            { text: "Frame Photos in your document", correct: false },
+            { text: "Embed a document within your HTML", correct: true },
+            { text: "Index your HTML 'p' tag for proper indentation", correct: false }
+        ],
+        answer: "Embed a document within your HTML"
     },
     {
-      question: "True or False: Media Queries allow a deployed website to adapt to multiple viewports?",
-      choices: [
-          {text: "True", correct: true},
-          {text:"False", correct: false},
-          {text: "Bro...what are you talking about?", correct: false}
-      ]
+        question: "Flexbox is used for which of the following purposes?",
+        choices: [
+            { text: "Designing flexible responsive layouts without using float or positioning", correct: true },
+            { text: "Automatically setting the height and width of a property", correct: false },
+            { text: "Adds a line-break after each element, so they can sit next to other elements", correct: false }
+        ],
+        answer: "Designing flexible responsive layouts without using float or positioning"
     },
     {
-      question: "Please identify the proper syntax for an Array.",
-      choices: [
-          {text: "var car = Ford, Chevy, Dodge;", correct: false},
-          {text: "var car = 'Ford' 'Chevy' 'Dodge';", correct: false},
-          {text: "var car = ['Ford','Chevy','Dodge'];", correct: true}
-      ]
+        question: "True or False: Media Queries allow a deployed website to adapt to multiple viewports?",
+        choices: [
+            { text: "True", correct: true },
+            { text: "False", correct: false },
+            { text: "Bro...what are you talking about?", correct: false }
+        ],
+        answer: "True"
     },
     {
-      question: "What is the definition of 'THIS'",
-      choices: [
-          {text: "THIS is a quiz", correct: false},
-          {text: "THIS is a programming language", correct: false},
-          {text: "THIS is the object from which the method was called", correct: true}
-      ]
+        question: "Please identify the proper syntax for an Array.",
+        choices: [
+            { text: "var car = Ford, Chevy, Dodge;", correct: false },
+            { text: "var car = 'Ford' 'Chevy' 'Dodge';", correct: false },
+            { text: "var car = ['Ford','Chevy','Dodge'];", correct: true }
+        ],
+        answer: "var car = ['Ford','Chevy','Dodge'];"
+    },
+    {
+        question: "What is the definition of 'THIS'",
+        choices: [
+            { text: "THIS is a quiz", correct: false },
+            { text: "THIS is a programming language", correct: false },
+            { text: "THIS is the object from which the method was called", correct: true }
+        ],
+        answer: "THIS is the object from which the method was called"
     }
 ]
